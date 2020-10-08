adcca1ddaad6350ced42843bed61695747fc1c3b
Fernando Ocaña <ferrubocana@gmail.com>
2020-10-08 07:16:42 -0600

    Reporte de examenes realizados en el día ya está implementado

--- Hospital/src/main/java/reportes/ReporteLaboratorista.java
+++ Hospital/src/main/java/reportes/ReporteLaboratorista.java
@@ -52,5 +52,40 @@ public class ReporteLaboratorista {
         
     }
 
+    public ArrayList<String[]> examenesRealizados(Connection connection, String codigoLaboratorista) {
+        ArrayList<String[]> lista = new ArrayList<String[]>();
+        String query = "SELECT codigo_examen FROM LABORATORISTA WHERE codigo = ?";
+        String query2 = "SELECT C.codigo,C.hora,P.codigo,P.nombre FROM CITA_LABORATORIO C INNER JOIN PACIENTE P ON P.codigo = C.codigo_paciente WHERE C.codigo_examen = ? AND C.hora < NOW() AND C.fecha = DATE(NOW())";
+        
+        try (PreparedStatement preSt = connection.prepareStatement(query);
+                PreparedStatement preSt2 = connection.prepareStatement(query2)) {
+            
+            preSt.setString(1, codigoLaboratorista);
+            ResultSet result = preSt.executeQuery();
+            result.next(); //Obtenemos el codigo de examen de laboratorio que realiza el laboratorista
+            
+            String codigoExamen = result.getString(1);
+            
+            preSt2.setString(1, codigoExamen);
+            ResultSet result2 = preSt2.executeQuery(); 
+            
+            while(result2.next()) { //Obtenemos los datos de los examenes que se realizan ese día
+                String[] datos = new String[4];
+                
+                datos[0] = result2.getString(1);
+                datos[1] = result2.getString(2);
+                datos[2] = result2.getString(3);
+                datos[3] = result2.getString(4);
+                
+                lista.add(datos);
+            }
+            
+            return lista;
+        } catch (Exception e) {
+            System.out.println("Error Examenes a Realizar: " + e.getMessage());
+            return null;
+        }
+        
+    }
     
 }
